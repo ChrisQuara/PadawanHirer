dc83f6bc4ad8e8bc6a5e2e230a9703645201ffc2
Magliolo Leonardo <36999122+MaglioloLeonardo@users.noreply.github.com>
2023-04-27 02:41:55 +0200

    Update Question.py

--- Question.py
+++ Question.py
@@ -1,18 +1,122 @@
+from abc import abstractmethod
+import random
+
+
 class Question:
     category = None
     weight = 0
     responses = None
+    isPartiallyCorrect = False
+    score = 0
+    maxAttempt = 1
 
-    def __init__(self, category, weight, responses):
+    def __init__(self, category, responses):
         self.category = category
-        self.weight = weight
-        if not isinstance(weight, int): raise RuntimeError('weight must be integer')
         self.responses = responses
 
-    def getScore(self):
-        # Da editare
-        return self.weight
+    @abstractmethod
+    def getScore(self, phrase_token):
+        pass
+
+    @abstractmethod
+    def askTheQuestion(self):
+        pass
 
     def toString(self):
         return "<Category:" + str(self.category) + ", Weight:" + str(self.weight) + ", Responses:" + str(
             self.responses) + ">"
+
+
+class QuestionSpecifyAllElements(Question):
+
+    def __init__(self, category, responses):
+        super().__init__(category, responses)
+        self.weight = 5
+        self.maxAttempt = 3
+
+    def getScore(self, phrase_words):
+        self.isPartiallyCorrect = False
+        to_respond = self.responses.copy()
+        for word in phrase_words:
+            if word in to_respond:
+                self.isPartiallyCorrect = True
+                to_respond.remove(word)
+        if (self.responses.__len__() - to_respond.__len__()) / self.responses.__len__() > 1 / 2:
+            self.score = self.weight
+        else:
+            self.score = 0
+        return self.score
+
+    def askTheQuestion(self):
+        print("Elenca tutte gli elementi della categoria '" + str(self.category) + "':")
+        return "Da implementare"
+
+
+class QuestionEnumerateElements(Question):
+    def __init__(self, category, responses):
+        super().__init__(category, responses)
+        self.weight = 3
+
+    def getScore(self, number_of_elements):
+        self.isPartiallyCorrect = False
+        if number_of_elements.__len__() > 1:
+            raise Exception("The list contains more than one element")
+        number_of_elements = int(number_of_elements[0])
+        number_of_responses = self.responses.__len__()
+        if number_of_elements >= 2 * number_of_responses or number_of_elements <= 0:
+            self.score = 0
+        else:
+            self.isPartiallyCorrect = True
+            self.score = round(
+                self.weight * (1 - (abs(number_of_responses - number_of_elements) / number_of_responses)))
+        return self.score
+
+    def askTheQuestion(self):
+        print("Quanti elementi contiene la categoria '" + str(self.category) + "':")
+        return "Da implementare"
+
+
+class QuestionYesOrNo(Question):
+    negation = False
+    elementToAsk = None
+
+    def __init__(self, category, real_responses, possible_responses):
+        super().__init__(category, real_responses)
+        self.weight = 2
+        self.negation = random.choice([True, False])
+        self.elementToAsk = random.choice(possible_responses)
+
+    def getScore(self, response):
+        self.isPartiallyCorrect = False
+        if response.__len__() > 1:
+            raise Exception("The list contains more than one element")
+        response = response[0].lower()
+
+        if response != "yes" and response != "no":
+            raise Exception("The response is not 'yes' or 'no'")
+
+        if (response.lower() == "yes" and not self.negation) or (response.lower() == "no" and self.negation):
+            for element in self.responses:
+                if element.lower() == self.elementToAsk.lower():
+                    self.isPartiallyCorrect = True
+                    self.score = self.weight
+                    return self.weight
+            return 0
+        elif (response.lower() == "no" and not self.negation) or (response.lower() == "yes" and self.negation):
+            for element in self.responses:
+                if element.lower() == self.elementToAsk.lower():
+                    self.score = 0
+                    return 0
+            self.isPartiallyCorrect = True
+            self.score = self.weight
+            return self.weight
+        else:
+            self.score = 0
+            return 0
+
+    def askTheQuestion(self):
+        negate = ""
+        if self.negation:
+            negate = " non "
+        print("E' vero che '" + self.elementToAsk + "'" + negate + "è membro della categoria '" + self.category + "':")
+        return "Da implementare"
